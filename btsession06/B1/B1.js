ad90a2364870c8dd99679922e092c10c640661f6
Doan01062002 <147359759+Doan01062002@users.noreply.github.com>
2024-04-08 10:25:42 +0700

    Add files via upload

---
+++ btsession06/B1/B1.js
@@ -0,0 +1,5 @@
+"use strict";
+var Name = 'Nguyễn Văn Đoan';
+var age = 20;
+var job = "student";
+console.log(Name, age, job);
---
+++ btsession06/B1/B1.ts
@@ -0,0 +1,5 @@
+const Name:string = 'Nguyễn Văn Đoan';
+const age:number = 20;
+const job:string = "student"
+
+console.log(Name, age, job);
