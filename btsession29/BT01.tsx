d096565ec30e4b46f9b28834a67ee816821d4376
Doan01062002 <147359759+Doan01062002@users.noreply.github.com>
2024-05-06 22:59:43 +0700

    btsession29

--- btsession29/BT01.tsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from 'react'
-
-export default function BT01() {
-  const [name, setName] = useState<string>("Nguyễn Minh Sơn")
-  return (
-    <div>Họ và tên: {name}</div>
-  )
-}
