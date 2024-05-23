c543108de560f17d4134ea1ae7e88dac616d00a8
Doan01062002 <147359759+Doan01062002@users.noreply.github.com>
2024-05-23 11:17:31 +0700

    B1.tsx

--- btsession41/B1.tsx
+++ btsession41/B1.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function B1() {
   return (
     <div>B1
-        <h1 className='text-red-500 text-2xl'>Tiến deep try</h1>
+        <h1 className='text-red-500 text-2xl'>Đoan</h1>
     </div>
   )
 }
